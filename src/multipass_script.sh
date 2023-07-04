f62bca0381f56c25c962205967689ce9f17d6f92
diyaz yakubov <diyaz.yakubov@disqo.com>
2023-07-04 22:10:55 +0300

    added creation of a cluster for k3s

--- src/multipass_script.sh
+++ src/multipass_script.sh
@@ -6,6 +6,18 @@ if ! command -v multipass &> /dev/null; then
     exit 1
 fi
 
+# Check if ssh-copy-id is installed
+if ! command -v ssh-copy-id &> /dev/null; then
+    echo "ssh-copy-id is not installed. Please install ssh-copy-id before proceeding."
+    exit 1
+fi
+
+# Check if k3sup is installed
+if ! command -v k3sup &> /dev/null; then
+    echo "k3sup is not installed. Please install k3sup before proceeding."
+    exit 1
+fi
+
 # Function to create or start VMs
 create_vms() {
     # Define an array of VM names
@@ -19,9 +31,37 @@ create_vms() {
             multipass start "$vm"
         else
             echo "Creating VM '$vm'..."
-            multipass launch --name "$vm" --cpus 1 --mem 256M --disk 3G
+            multipass launch --name "$vm" --cpus 2 --mem 512M --disk 3G --cloud-init cloud-init.yaml
         fi
     done
+}
+
+setup_cluster(){
+    # Retrieve the list of Multipass VM names
+    vm_names=$(multipass list --format csv | awk -F ',' 'NR>1{print $1}')
+
+    # Retrieve the IP addresses of Multipass VMs
+    ip_addresses=()
+    for vm in $vm_names
+    do
+        ip=$(multipass info $vm | awk '/IPv4/{print $2}')  # Retrieve IP address of the VM
+        ip_addresses+=($ip)
+    done
+
+    # Set the first VM as the master node
+    master_ip=${ip_addresses[0]}
+    echo "Installing K3s on the master node with IP '$master_ip'..."
+    k3sup install --ip $master_ip --user root --ssh-key $HOME/.ssh/iot-edge
+
+    # Join the remaining VMs as worker nodes
+    worker_ips=("${ip_addresses[@]:1}")  # Exclude the first IP address
+    for worker_ip in "${worker_ips[@]}"
+    do
+        echo "Installing K3s on worker node at $worker_ip and joining to the master node..."
+        k3sup join --ip $worker_ip --server-ip $master_ip --user root --ssh-key $HOME/.ssh/iot-edge
+    done
+
+    echo "K3s cluster setup complete!"
 }
 
 # Function to stop VMs
@@ -41,7 +81,8 @@ do
     echo "1. Launch/Start VMs"
     echo "2. Stop VMs"
     echo "3. Remove VMs"
-    echo "4. Exit"
+    echo "4. Install k3s"
+    echo "5. Exit"
 
     read -p "Option: " option
 
@@ -55,7 +96,10 @@ do
         3)
             remove_vms
             ;;
-        4)
+        4) 
+            setup_cluster
+            ;;
+        5)
             echo "Exiting..."
             exit 0
             ;;
