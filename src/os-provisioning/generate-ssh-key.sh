fbd3b12855e53e3c9de24d4f62ab8867078173db
diyaz yakubov <diyaz.yakubov@disqo.com>
2023-07-22 13:12:52 +0300

    automated the ssh keys generation

---
+++ src/os-provisioning/generate-ssh-key.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+generate_ssh_key() {
+    # This function expects the key name as its first argument
+    local key_name="$1"
+    # This function expects the path as its second argument
+    local ssh_path="${2:-$HOME/.ssh}"
+    # This function expects a flag indicating whether to overwrite existing keys as its third argument
+    local overwrite="$3"
+
+
+    # Check if a key name was provided
+    if [[ -z "$key_name" ]]; then
+        echo "Please provide a key name as the first argument."
+        return 1
+    fi
+
+    # Ensure the path exists
+    mkdir -p "$ssh_path"
+
+    # Check if the key already exists
+    if [[ -f "$ssh_path/$key_name" && "$overwrite" || ! -f "$ssh_path/$key_name" ]]; then
+        echo "Generating a new key..."
+        ssh-keygen -t rsa -b 4096 -f "$ssh_path/$key_name" -q -N ""
+    fi
+
+    # Output the public key
+    echo "Copy the following public key to your cloud-init config:"
+    cat "$ssh_path/${key_name}.pub"
+}
