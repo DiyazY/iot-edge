40ef9e4ed07fa0e894b1f1d1256373df0127904e
DiyazY <diyaz.yakubov@gmail.com>
2023-12-24 22:35:12 +0200

    improved script for automation

--- src/ansible/playbooks/run-reliability-tests.sh
+++ src/ansible/playbooks/run-reliability-tests.sh
@@ -3,7 +3,7 @@
 # WIP
 
 distribution="k0s"
-test_types=("reliability-control" "reliability-worker" "reliability-control-no-pressure-long" "reliability-worker-no-pressure-long")
+test_types=("dp_redis_density" "reliability-control" "reliability-worker" "reliability-control-no-pressure-long" "reliability-worker-no-pressure-long")
 
 
 for test_type in "${test_types[@]}"; do
@@ -20,34 +20,63 @@ for test_type in "${test_types[@]}"; do
         sleep 1800 # wait 30 min
 
         machine="control"
+        eth_interface="eno1"
         if [[ "$test_type" == *"worker"* ]]; then
             machine = "worker"
+            eth_interface="eth0"
         fi
 
         if [[ "$test_type" == "idle" ]]; then
             start_time=$(date '+%s')
-            echo "start time: $start_time"
+            echo "$start_time" > ../k-bench-results/${distribution}/${tag}/tmp-before.txt
             sleep 300 # wait 5 mins
             end_time=$(date '+%s')
-            echo "end time: $end_time"
+            echo "$end_time" >> ../k-bench-results/${distribution}/${tag}/tmp-after.txt
             ansible-playbook -i inventory/${distribution}/hosts.ini ./playbooks/mongodb-derive-data.yaml --extra-vars "start_test=${start_time} end_test=${end_time} test_type=${test_type}" >> "$output_file" 2>&1 
         elif [[ "$test_type" == "reliability-control" || "$test_type" == "reliability-worker" ]]; then
+            random_number=$((RANDOM % 3))
+            if [["$machine" == "control"]]; then
+                random_number = 0
+            fi
+            sleep_time=100
+            wait_time=600
+            echo "Start testing of $machine: $random_number" > "$output_file"
             ansible-playbook -i inventory/${distribution}/hosts.ini ./playbooks/k-bench-run.yaml --extra-vars "test_type=dp_redis_density" > "${output_file}-density" 2>&1 &
-            ansible-playbook -i inventory/${distribution}/hosts.ini ./playbooks/reliability-${machine}-test.yaml --extra-vars "node_index=0" > "$output_file" 2>&1
-            # TODO: gather metrics from mongo
-            # seems running worker and control together a bit difficult because (1 control, 3 nodes)
-        elif [[ "$test_type" == "" ]]; then
+            ansible-playbook -i inventory/${distribution}/hosts.ini ./playbooks/reliability-test.yaml --extra-vars "node_index=$random_number machine=$machine eth_interface=$eth_interface" >> "$output_file" 2>&1
+
+            start_time=$(cat ../k-bench-results/${distribution}/${test_type}/${tag}/tmp-before.txt)
+            echo "start time: $start_time"
+            end_time=$(cat ../k-bench-results/${distribution}/${test_type}/${tag}/tmp-after.txt)
+            echo "end time: $end_time"
+            ansible-playbook -i inventory/${distribution}/hosts.ini ./playbooks/mongodb-derive-data.yaml --extra-vars "start_test=${start_time} end_test=${end_time} test_type=${test_type}" >> "$output_file" 2>&1 
+        elif [[ "$test_type" == "reliability-control-no-pressure-long" || "$test_type" == "reliability-worker-no-pressure-long" ]]; then
+            random_number=$((RANDOM % 3))
+            if [["$machine" == "control"]]; then
+                random_number = 0
+            fi
+            sleep_time=500
+            wait_time=200
+
+            echo "Start testing of $machine: $random_number" > "$output_file"
+            ansible-playbook -i inventory/${distribution}/hosts.ini ./playbooks/k-bench-run.yaml --extra-vars "test_type=dp_redis_density" > "${output_file}-density" 2>&1 &
+            ansible-playbook -i inventory/${distribution}/hosts.ini ./playbooks/reliability-test.yaml --extra-vars "node_index=$random_number machine=$machine eth_interface=$eth_interface" >> "$output_file" 2>&1
+
+            start_time=$(cat ../k-bench-results/${distribution}/${test_type}/${tag}/tmp-before.txt)
+            echo "start time: $start_time"
+            end_time=$(cat ../k-bench-results/${distribution}/${test_type}/${tag}/tmp-after.txt)
+            echo "end time: $end_time"
+            ansible-playbook -i inventory/${distribution}/hosts.ini ./playbooks/mongodb-derive-data.yaml --extra-vars "start_test=${start_time} end_test=${end_time} test_type=${test_type}" >> "$output_file" 2>&1 
         else
             ansible-playbook -i inventory/${distribution}/hosts.ini ./playbooks/k-bench-run.yaml --extra-vars "test_type=${test_type}" >> "$output_file">> "$output_file" 2>&1
-            start_time=$(cat ./playbooks/tmp-before.txt)
+            start_time=$(cat ../k-bench-results/${distribution}/${test_type}/${tag}/tmp-before.txt)
             echo "start time: $start_time"
-            end_time=$(cat ./playbooks/tmp-after.txt)
+            end_time=$(cat ../k-bench-results/${distribution}/${test_type}/${tag}/tmp-after.txt)
             echo "end time: $end_time"
             ansible-playbook -i inventory/${distribution}/hosts.ini ./playbooks/mongodb-derive-data.yaml --extra-vars "start_test=${start_time} end_test=${end_time} test_type=${test_type}" >> "$output_file" 2>&1 
         fi
 
         if [[ "$test_type" == "dp_redis_density" || "$test_type" == "reliability-control" || "$test_type" == "reliability-worker" ]]; then
-            kubectl cp kbench-pod-namespace/kbench-pod-oid-0-tid-0:tmp/redisoutput ../../k-bench-results/${distribution}/${test_type}/${tag}/ --kubeconfig ../../.kube/${distribution}-config
+            kubectl cp kbench-pod-namespace/kbench-pod-oid-0-tid-0:tmp/redisoutput ../k-bench-results/${distribution}/${test_type}/${tag}/ --kubeconfig ../.kube/${distribution}-config
         fi
 
         # TODO: check step
@@ -56,6 +85,10 @@ for test_type in "${test_types[@]}"; do
         git add "../../."
         git commit -m"${distribution} | ${tag}"
     done
+    # remove containers for redis tests
+    if [[ "$test_type" == "dp_redis_density" || "$test_type" == "reliability-control" || "$test_type" == "reliability-worker" ]]; then
+        ansible-playbook -i inventory/${distribution}/hosts.ini ./playbooks/clean-redis-containers.yaml >> "$output_file" 2>&1 
+    fi
     echo "waiting 30min..."
     sleep 1800 # wait 30 min
 done
