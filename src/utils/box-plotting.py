296f53b044f2484925bb5df3f59c769a421fa873
diyaz yakubov <diyaz.yakubov@disqo.com>
2024-04-04 18:42:49 +0300

    disk | box without fliers | workersOnly

--- src/utils/box-plotting.py
+++ src/utils/box-plotting.py
@@ -137,7 +137,7 @@ def create_plots(files, title, xlabel, ylabel, toSave=False, plot_type='scatter'
         all_data.append(data)
 
     if plot_type == 'box':
-        box_plotting(all_data, title, xlabel, ylabel, toSave, unit, showfliers=True)
+        box_plotting(all_data, title, xlabel, ylabel, toSave, unit, showfliers=False)
     elif plot_type == 'line':
         line_plotting(all_data, title, xlabel, ylabel, toSave, unit)
     else:
