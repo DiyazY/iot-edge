88ad607ec0e522fa4bf92adc7b0defc745785022
diyaz yakubov <diyaz.yakubov@disqo.com>
2024-04-14 23:06:43 +0300

    spider diagram is done

--- src/utils/box-plotting.py
+++ src/utils/box-plotting.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+from matplotlib.patches import Circle, RegularPolygon
+from matplotlib.path import Path
+from matplotlib.projections import register_projection
+from matplotlib.projections.polar import PolarAxes
+from matplotlib.spines import Spine
+from matplotlib.transforms import Affine2D
 
 def scatter_plots_with_trend_lines(all_data, title, xlabel, ylabel, toSave=False):
     combined_data = pd.concat(all_data, ignore_index=True)
@@ -169,4 +179,198 @@ def create_plots_time_series(plot_type='scatter'):
             ylabel = 'CPU Usage (%)' if unit == 'cpu' else 'Memory Usage (Mb)' if unit == 'ram' else 'Network load (kB)' if unit == 'net' else 'Disk Usage (%)'
             create_plots(files, f'{test}', 'Minutes', ylabel, toSave, plot_type, uniteWorkers, reliabilityTestsForWorker)
 
-create_plots_time_series('line')
+# create_plots_time_series('line')
+
+
+
+
+######## spider plots below
+def radar_factory(num_vars, frame='circle'):
+    """
+    Create a radar chart with `num_vars` axes.
+
+    This function creates a RadarAxes projection and registers it.
+
+    Parameters
+    ----------
+    num_vars : int
+        Number of variables for radar chart.
+    frame : {'circle', 'polygon'}
+        Shape of frame surrounding axes.
+
+    """
+    # calculate evenly-spaced axis angles
+    theta = np.linspace(0, 2*np.pi, num_vars, endpoint=False)
+
+    class RadarTransform(PolarAxes.PolarTransform):
+
+        def transform_path_non_affine(self, path):
+            # Paths with non-unit interpolation steps correspond to gridlines,
+            # in which case we force interpolation (to defeat PolarTransform's
+            # autoconversion to circular arcs).
+            if path._interpolation_steps > 1:
+                path = path.interpolated(num_vars)
+            return Path(self.transform(path.vertices), path.codes)
+
+    class RadarAxes(PolarAxes):
+
+        name = 'radar'
+        PolarTransform = RadarTransform
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            # rotate plot such that the first axis is at the top
+            self.set_theta_zero_location('N')
+
+        def fill(self, *args, closed=True, **kwargs):
+            """Override fill so that line is closed by default"""
+            return super().fill(closed=closed, *args, **kwargs)
+
+        def plot(self, *args, **kwargs):
+            """Override plot so that line is closed by default"""
+            lines = super().plot(*args, **kwargs)
+            for line in lines:
+                self._close_line(line)
+
+        def _close_line(self, line):
+            x, y = line.get_data()
+            # FIXME: markers at x[0], y[0] get doubled-up
+            if x[0] != x[-1]:
+                x = np.append(x, x[0])
+                y = np.append(y, y[0])
+                line.set_data(x, y)
+
+        def set_varlabels(self, labels):
+            self.set_thetagrids(np.degrees(theta), labels)
+
+        def _gen_axes_patch(self):
+            # The Axes patch must be centered at (0.5, 0.5) and of radius 0.5
+            # in axes coordinates.
+            if frame == 'circle':
+                return Circle((0.5, 0.5), 0.5)
+            elif frame == 'polygon':
+                return RegularPolygon((0.5, 0.5), num_vars,
+                                      radius=.5, edgecolor="k")
+            else:
+                raise ValueError("Unknown value for 'frame': %s" % frame)
+
+        def _gen_axes_spines(self):
+            if frame == 'circle':
+                return super()._gen_axes_spines()
+            elif frame == 'polygon':
+                # spine_type must be 'left'/'right'/'top'/'bottom'/'circle'.
+                spine = Spine(axes=self,
+                              spine_type='circle',
+                              path=Path.unit_regular_polygon(num_vars))
+                # unit_regular_polygon gives a polygon of radius 1 centered at
+                # (0, 0) but we want a polygon of radius 0.5 centered at (0.5,
+                # 0.5) in axes coordinates.
+                spine.set_transform(Affine2D().scale(.5).translate(.5, .5)
+                                    + self.transAxes)
+                return {'polar': spine}
+            else:
+                raise ValueError("Unknown value for 'frame': %s" % frame)
+
+    register_projection(RadarAxes)
+    return theta
+
+
+def return_single_avg_value(files, metric, machine):
+    all_data = []
+    avg_value = 0
+    for file in files:
+        data = pd.read_csv(file)
+        if metric == 'cpu':
+            data['value'] = 100 - data['value']
+        if metric == 'net':
+            data['value'] = data['value'].abs()
+            # Group by 'hostname' and 'timestamp' to calculate total traffic (sum of InOctets and OutOctets)
+            data_grouped = data.groupby(['hostname', 'timestamp']).agg(TotalTraffic=('value', 'sum')).reset_index()
+
+            # Replace the original 'data' DataFrame with the grouped one
+            data = data_grouped
+            data['value'] = data['TotalTraffic'] / 1024  # Convert bytes to kilobytes
+
+        if machine == 'worker':
+            data = data[data['hostname'].str.match('^node_.*')].copy()
+        else:
+            data = data[data['hostname'].str.match('^master$')].copy()       
+        all_data.append(data)
+    combined_data = pd.concat(all_data, ignore_index=True)
+    avg_value = combined_data['value'].mean()
+    if (metric == 'ram'):
+        if machine == 'worker':
+            avg_value =( avg_value * 100) / 4096
+        else:
+            avg_value  = (avg_value * 100) / 65336
+    # if (metric == 'net'):
+    #     avg_value = (avg_value * 100) / 3
+    return avg_value
+    
+
+def prepare_data_set_for_machine(machine):
+    data = dict()
+    for test in testCases:
+        data[test] = dict()
+        for dist in distributions:
+            data[test][dist] = []
+            for metric in metrics:
+                files = []
+                for i in range(2, 5):
+                    files.append(f'../k-bench-results/{dist}/{test}/{test}-{i}/{test}-{i}-{metric}.csv')
+                avg = return_single_avg_value(files, metric, machine)
+                # print(f'{test} for {dist} and {metric} is {avg}')
+                data[test][dist].append(avg)
+    return data
+
+
+
+
+def create_spider_plots_for_test_cases(toSave=False):
+
+    data_set = [prepare_data_set_for_machine('master'), prepare_data_set_for_machine('worker')]
+    
+    N = len(metrics)
+    theta = radar_factory(N, frame='polygon')
+    spoke_labels = metrics
+
+    fig, axs = plt.subplots(figsize=(10, 35), nrows=len(testCases), ncols=2, # each distribution should be presented as two diagrams: master and worker
+                            subplot_kw=dict(projection='radar'))
+    fig.subplots_adjust(wspace=0.25, hspace=0.20, top=0.90, bottom=0.05)
+    colors = ['b', 'r', 'g', 'm', 'y']
+
+    # I want to have plots a bit far from each other
+    fig.tight_layout(pad=3.0)
+
+    for p, data in enumerate(data_set):
+        for i, (test, dists) in enumerate(data.items()):
+            max_value = 0
+            for j, (dist, values) in enumerate(dists.items()):
+                # if (i >= 2 or j >= 2):
+                #     print(f' i = {i} and j = {j}, test = {test} and dist = {dist}')
+                if (len(values) == 0):
+                    print(f'No data for {test} and {dist}')
+                    continue
+                # print(values)
+                axs[i, p].plot(theta, values, color=colors[j])
+                axs[i, p].fill(theta, values, facecolor=colors[j], alpha=0.5)   
+                max_value = max(max_value, max(values)) 
+            
+            axs[i, p].set_varlabels(spoke_labels)
+            noteType = 'Master' if p == 0 else 'Worker'
+            axs[i, p].set_title(f'{test} for {noteType} (%)')
+            axs[i, p].set_ylim(0, math.ceil(max_value))
+
+    legend = axs[0, 0].legend(distributions, loc=(1.2, .80),
+                              labelspacing=0.2, fontsize='small')
+
+    # fig.text(0.5, 0.965, 'Some descriptive title',
+    #          horizontalalignment='center', color='black', weight='bold',
+    #          size='large')
+
+    if toSave:
+            plt.savefig(f'../diagrams/spider_diagrams.png')
+    else:
+        plt.show()
+    
+create_spider_plots_for_test_cases(True)
